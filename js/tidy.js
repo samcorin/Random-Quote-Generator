626f479b9091a74389562254d9c74359827415d9
Sam Corin <samcorin@gmail.com>
2015-11-17 00:54:14 +0900

    Initial commit

---
+++ js/tidy.js
@@ -0,0 +1,7 @@
+// here we tidy up the quotes.
+// should also clean up the language if necessary.
+
+
+function pg13(str) {
+	return;
+}
---
+++ js/timer.js
@@ -0,0 +1,12 @@
+// Excecution time methods:
+// 1
+// var start = new Date().getTime();
+// var end = new Date().getTime();
+// var time = end - start;
+// console.log('Execution time: ' + time);
+
+// 2
+// var t0 = performance.now();
+// doSomething();
+// var t1 = performance.now();
+// console.log("Call to doSomething took " + (t1 - t0) + " milliseconds.")
